aeba0688f286d03ca6ad60e36b40a9ec39c0083c
gotoERROR00111011 <gotoerror00111011@gmail.com>
2019-05-05 07:43:06 +0900

    Update init.js

--- 2017-2/The10000HourRule/public/js/init.js
+++ 2017-2/The10000HourRule/public/js/init.js
@@ -1,10 +1,10 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyCaVdkTLXY-nqcir3ty3TUYiLZdRdPiOyU",
-  authDomain: "fir-201464032.firebaseapp.com",
-  databaseURL: "https://fir-201464032.firebaseio.com",
-  projectId: "fir-201464032",
-  storageBucket: "fir-201464032.appspot.com",
-  messagingSenderId: "633165607736"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 firebase.initializeApp(config);
